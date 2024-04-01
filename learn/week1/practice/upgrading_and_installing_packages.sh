3cfd6d11c592d842794668c9cc8ccf44457aed23
fauzan264 <fauzan.c4@gmail.com>
2024-04-02 02:28:23 +0700

    upgrading_and_installing_packages

---
+++ learn/week1/practice/upgrading_and_installing_packages.sh
@@ -0,0 +1,9 @@
+# update package list
+sudo apt update
+
+# upgrading nano
+sudo apt upgrade nano
+
+# installing vim
+# Because Vim isn't preinstalled on your Linux system, you'll need to install it yourself. If you haven't already done so in this session, ensure you run the command sudo apt update. Then to install Vim, enter the following command:
+sudo apt install vim
