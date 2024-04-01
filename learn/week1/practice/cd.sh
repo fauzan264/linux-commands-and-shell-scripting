8909494ddf2f1547b78afc6cae268815f14e24f9
fauzan264 <fauzan.c4@gmail.com>
2024-04-02 01:43:39 +0700

    practice change directory or navigation directory

---
+++ learn/week1/practice/cd.sh
@@ -0,0 +1,16 @@
+# Exercise 2 - Navigating Directories
+# cd
+
+# In this exercise, you will explore directories using the cd command.
+# Recall the symbols used to navigate to special paths:
+# ~	Home directory
+cd ~
+
+# /	Root directory
+cd /
+
+# .	Current directory
+cd .
+
+# ..   	Parent directory
+cd ..
