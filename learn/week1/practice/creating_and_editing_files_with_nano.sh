1ba6b5ee737bcd8933224cbb8a74428c21413fc5
fauzan264 <fauzan.c4@gmail.com>
2024-04-02 02:27:47 +0700

    creating_and_editing_files_with_nano

---
+++ learn/week1/practice/creating_and_editing_files_with_nano.sh
@@ -0,0 +1,16 @@
+# Navigating to the project directory
+# We provide you with an empty project directory at /home/project.
+cd /home/project
+
+# Creating and editing a text file with nano
+nano hello_world.txt
+
+# In your nano terminal, whatever you type will be added to your text buffer, where text is stored until you save it. Type the following text in your nano terminal:
+# This will create the text Hello world! in your text buffer.
+# To create another line of text, press Enter. In your new line, type
+
+# Hello world!
+# This is the second line of my first-ever text file created with nano.
+
+# Verifying your new text file
+cat hello_world.txt
