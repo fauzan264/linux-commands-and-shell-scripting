7072da0cae434bfee9439dcff875db22c2d9f04d
fauzan264 <fauzan.c4@gmail.com>
2024-04-02 02:28:43 +0700

    practice2

---
+++ learn/week1/practice/practice2.sh
@@ -0,0 +1,20 @@
+
+
+nano hello_world.txt
+
+# This is line three of my new file.
+# Press CTRL + x to exit, save y and Enter
+
+# Verifying hello_world.txt
+cat hello_world.txt
+
+vim done.txt
+# press I 
+
+# When finished, you should see the following text echoed to your terminal:
+# I am done with the lab!
+
+# :wq and presssing Enter
+
+# Verifying done.txt
+cat done.txt
