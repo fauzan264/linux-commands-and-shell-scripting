491a4a1a4903b64460612ace42d5bad8d15684ca
fauzan264 <fauzan.c4@gmail.com>
2024-04-02 02:28:07 +0700

    creating_and_editing_files_with_vim

---
+++ learn/week1/practice/creating_and_editing_files_with_vim.sh
@@ -0,0 +1,14 @@
+# Creating and editing a text file with Vim
+vim hello_world_2.txt
+
+# press i to insert mode. This is the mode where you can enter and delete text in the text buffer.
+# Hello World!
+# This is the second line.
+
+# When you're done typing text in the buffer, press the Escape key, Esc, to exit the Insert mode. This brings you to Command mode.
+
+# Now that you are back in Command mode, you can save your work by entering the command :w. This writes the contents of the text buffer to your text file.
+:w
+
+# quit
+:q
