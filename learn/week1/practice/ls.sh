bb64c49e227f287d3f881c027788b8fe507ab30b
fauzan264 <fauzan.c4@gmail.com>
2024-04-02 01:44:18 +0700

    display all files

---
+++ learn/week1/practice/ls.sh
@@ -0,0 +1,21 @@
+# Typing and entering ls will display all files and directories contained within your current working directory:
+# If you know the path to a directory, you can view its contents by passing the path name as a command line argument to the ls command as follows:
+
+# ls [PATH TO DIRECTORY]
+# For example:
+# ls /
+
+# /bin: System commands, also called binaries
+ls /bin
+
+# /sbin: System administration binaries
+ls /sbin
+
+# /usr: User programs and data
+ls /usr
+
+# /home: Home directory
+ls /home
+
+# /media: Removable media device directories
+ls /media
