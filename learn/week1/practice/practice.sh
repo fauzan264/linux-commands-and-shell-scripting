ee27695d5fbf670ee72181a31a45bd6dea19381c
fauzan264 <fauzan.c4@gmail.com>
2024-04-02 01:46:44 +0700

    Hands-on Lab: Getting Started with the Linux Terminal

---
+++ learn/week1/practice/practice.sh
@@ -0,0 +1,16 @@
+# 1. List the contents of the root directory.
+ls /
+
+# 2. Change directories to your default home directory.
+cd ~
+
+# 3. Verify your current working directory is /home/theia.
+pwd
+
+# 4. Use tab completion to change directories to /bin.
+cd ~
+cd /bi
+# press tab
+
+# 5. Use your terminal's command history to change directories back to your home directory.
+# use up arrow
